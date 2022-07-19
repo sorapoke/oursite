16aee3e67a3e060b23d178cb8909faaffcc17bc9
sorapoke <sorposoul2r@gmail.com>
2022-07-19 16:09:22 +0900

    nextconfig

--- src/next.config.js
+++ src/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
-}
 
-module.exports = nextConfig
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+}
